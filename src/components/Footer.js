696784a8bb5c4fcc104c96b834b7bfab1ccb8375
Tong Yin <71139027+TongYinn@users.noreply.github.com>
2023-09-02 22:18:50 -0400

    footer css

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import "../index.css";
+import "../components/Footer.scss";
+import Box from '@material-ui/core/Box';
+import resume from '../assets/Tong Yin Han - PM Resume S24.pdf'
 
 const Footer = () => {
     return (
-        <a>made with love</a>
+        <Box className="footerContainer" >
+            <Box justifyContent="space-evenly">
+                <a target="_blank" className="link" href="https://github.com/TongYinn" rel="noopener noreferrer">Github</a>
+                <a target="_blank" className="link" href="mailto:tyhan@uwaterloo.ca" rel="noopener noreferrer">Email</a>
+                <a target="_blank" className="link" href={resume} rel="noopener noreferrer">Resume</a>
+            </Box>
+            <Box className="credit"> designed and developed by Tong Yin Han</Box>
+        </Box>
     );
   };
   
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,9 +2,9 @@ import React from "react";
 
 const Home = () => {
     return (
-        <div>
+        <a>
             Hi there! I'm Tong Yin
-        </div>
+        </a>
     );
 };
 
