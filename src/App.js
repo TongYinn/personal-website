d986c1792bdcfb0899ddceb7d7c74135c8afcbc0
Tong Yin <71139027+TongYinn@users.noreply.github.com>
2023-09-04 22:14:53 -0400

    deploy website

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import './App.css';
 import React from 'react';
-import { Router, Routes, Route} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import NavBar from './components/NavBar';
 import Footer from './components/Footer';
 import Home from "./pages/Home";
 import About from "./pages/About";
-import { Navigate } from "react-router-dom";
-
 
 function App() {
   return (
-
     <React.Fragment>
         <Home />
         <About />
-        <Routes>
-          <Route path="/" component={Home} />
-          <Route path="/about" component={About} />
-          <Route path="/" element={<Navigate replace to="/home" />} />
-        </Routes>
         <Footer />
    </React.Fragment>
   );
